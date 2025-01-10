10b074bf9129e2f6b80bd9734866e4c736983b17
shupimumi <shupimumi@gmail.com>
2025-01-10 23:13:06 +0300

    update

--- src/main/java/com/home/marketplace/db/repositories/OrderRepository.java
+++ src/main/java/com/home/marketplace/db/repositories/OrderRepository.java
@@ -3,5 +3,8 @@ package com.home.marketplace.db.repositories;
 import com.home.marketplace.db.entities.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    Set<OrderEntity> findBy
 }
