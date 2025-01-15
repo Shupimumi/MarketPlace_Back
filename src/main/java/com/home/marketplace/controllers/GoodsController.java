265acb47314e07c81b86e624e6af7222a4217a02
shupimumi <shupimumi@gmail.com>
2025-01-15 19:58:29 +0300

    feature/MP-0008: update properties for spring security

--- src/main/java/com/home/marketplace/controllers/GoodsController.java
+++ src/main/java/com/home/marketplace/controllers/GoodsController.java
@@ -7,6 +7,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
@@ -26,6 +27,7 @@ public class GoodsController {
 
     private final GoodEntityModelAssembler assembler;
 
+    @Autowired
     GoodsController(GoodEntityModelAssembler assembler) {
         this.assembler = assembler;
     }
