39ff9888fa064b278b350d91a750a3155084a495
shupimumi <shupimumi@gmail.com>
2025-02-09 13:52:54 +0400

    Update spring  security

--- src/main/java/com/home/marketplace/db/repositories/OrderRepository.java
+++ src/main/java/com/home/marketplace/db/repositories/OrderRepository.java
@@ -3,8 +3,5 @@ package com.home.marketplace.db.repositories;
 import com.home.marketplace.db.entities.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Set;
-
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-    Set<OrderEntity> findBy
 }
