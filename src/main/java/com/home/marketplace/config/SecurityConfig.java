a07dc91f04b971afad34e3a4937561e7b6f8b6af
shupimumi <shupimumi@gmail.com>
2025-02-11 20:58:51 +0300

    Resource server on keycloak with spring was done

--- src/main/java/com/home/marketplace/config/SecurityConfig.java
+++ src/main/java/com/home/marketplace/config/SecurityConfig.java
@@ -6,18 +6,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.stream.Stream;
 
 @Configuration
 @EnableWebSecurity
@@ -34,7 +23,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
+    /*@Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
@@ -51,22 +40,22 @@ public class SecurityConfig {
                     .toList();
         });
         return jwtAuthenticationConverter;
-    }
-
-    @Bean
-    public OAuth2UserService<OidcUserRequest, OidcUser> oAuth2UserService() {
-        var oidcUserService = new OidcUserService();
-        return userRequest -> {
-            var oidcUser = oidcUserService.loadUser(userRequest);
-            var roles = oidcUser.getClaimAsStringList("spring_sec_roles");
-            var authorities = Stream.concat(oidcUser.getAuthorities().stream(),
-                            roles.stream()
-                                    .filter(role -> role.startsWith("ROLE_"))
-                                    .map(SimpleGrantedAuthority::new)
-                                    .map(GrantedAuthority.class::cast))
-                    .toList();
-
-            return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
-        };
-    }
+    }*/
+//
+//    @Bean
+//    public OAuth2UserService<OidcUserRequest, OidcUser> oAuth2UserService() {
+//        var oidcUserService = new OidcUserService();
+//        return userRequest -> {
+//            var oidcUser = oidcUserService.loadUser(userRequest);
+//            var roles = oidcUser.getClaimAsStringList("spring_sec_roles");
+//            var authorities = Stream.concat(oidcUser.getAuthorities().stream(),
+//                            roles.stream()
+//                                    .filter(role -> role.startsWith("ROLE_"))
+//                                    .map(SimpleGrantedAuthority::new)
+//                                    .map(GrantedAuthority.class::cast))
+//                    .toList();
+//
+//            return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+//        };
+//    }
 }
